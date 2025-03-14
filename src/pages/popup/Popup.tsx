8c6eaffe1269a3f5ca769c80a60bc6c9dc117838
Dabemuc <daniel.benner02@gmail.com>
2025-03-14 16:02:51 +0100

    Added label for send-prompt-instantly-checkbox

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -122,26 +122,30 @@ export default function Popup() {
     return (
       <div className="relative flex items-center w-full">
         <div className="p-2">
-          <input
-            type="checkbox"
-            title="Instantly send prompts"
-            checked={settings?.send_instantly.value}
-            onChange={() =>
-              setSettings((prevSettings) => {
-                if (prevSettings) {
-                  return {
-                    ...prevSettings,
-                    send_instantly: {
-                      ...prevSettings.send_instantly,
-                      value: !prevSettings.send_instantly.value,
-                      label: prevSettings.send_instantly.label,
-                    },
-                  };
-                }
-                return prevSettings;
-              })
-            }
-          />
+          <label className="flex items-center">
+            <input
+              className="mx-2"
+              type="checkbox"
+              title="Instantly send prompts"
+              checked={settings?.send_instantly.value}
+              onChange={() =>
+                setSettings((prevSettings) => {
+                  if (prevSettings) {
+                    return {
+                      ...prevSettings,
+                      send_instantly: {
+                        ...prevSettings.send_instantly,
+                        value: !prevSettings.send_instantly.value,
+                        label: prevSettings.send_instantly.label,
+                      },
+                    };
+                  }
+                  return prevSettings;
+                })
+              }
+            />
+            {"Send prompts on click"}
+          </label>
         </div>
         <span className="absolute left-1/2 transform -translate-x-1/2 p-2"></span>
         <div className="p-2 ml-auto"></div>
